3630ad6f850e6a96e30ab0aabb3dcfffae543756
LouisDuv <92506934+LouisDuv@users.noreply.github.com>
2022-11-27 20:49:24 +0100

    Add files via upload

---
+++ menuDebut.h
@@ -0,0 +1,13 @@
+//
+// Created by Louis Duvanel on 02/11/2022.
+//
+
+#ifndef PROJET_ECE_CITY_MENUDEBUT_H
+#define PROJET_ECE_CITY_MENUDEBUT_H
+
+#include "include.h"
+
+
+void menuDebut(Principal* p);
+
+#endif //PROJET_ECE_CITY_MENUDEBUT_H
